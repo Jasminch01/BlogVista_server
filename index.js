e2aa51eb2856216ed68a7b2e7754edc78aa0138b
jasmin <jasminchakma895@gmail.com>
2024-03-05 12:38:01 +0600

    update all apis

--- index.js
+++ index.js
@@ -1,24 +1,85 @@
-const express = require('express')
-const cors = require('cors')
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express()
-const PORT = process.env.PORT | 5000;
-
+const app = express();
+const PORT = process.env.PORT || 5000;
 
 //middlewares
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('blogVista server is runnig')
-})
+const uri = process.env.URI;
 
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
-app.get('/helo', (req, res) => {
-    res.send('hello world')
-})
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
-app.listen(PORT ,() => {
-    console.log(`the blogVista server is runing port is ${PORT}`)
-})
+    //db collections
+    const wishlistCollection = client.db("BlogVista").collection("wishlist");
+    const allBlogsCollection = client.db("BlogVista").collection("allBlogs");
+
+    //get api's
+    app.get("/allBlogs", async (req, res) => {
+      const result = await allBlogsCollection.find().toArray();
+      res.send(result);
+    });
+    app.get('/recentBlogs', async(req, res) => {
+      const sort = {createdAt : - 1}
+      const result = await allBlogsCollection.find().sort(sort).limit(6).toArray();
+      res.send(result)
+    })
+    app.get("/allBlogs/:id", async (req, res) => {
+      const id = req.params.id;
+      const blogId = {_id : new ObjectId(id)}
+      const result = await allBlogsCollection.findOne(blogId)
+      res.send(result);
+    });
+    app.get("/wishlist", async (req, res) => {
+      const result = await wishlistCollection.find().toArray();
+      res.send(result);
+    });
+
+    //post api's
+    app.post("/addblogs", async(req, res) => {
+      const blog = req.body;
+      blog.createdAt = new Date()
+      const result = await  allBlogsCollection.insertOne(blog)
+      res.send(result)
+    })
+    app.post("/wishlist", async (req, res) => {
+      const wishBlog = req.body;
+      const result = await wishlistCollection.insertOne(wishBlog);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("blogVista server is runnig");
+});
+
+app.listen(PORT, () => {
+  console.log(`the blogVista server is runing port is ${PORT}`);
+});
